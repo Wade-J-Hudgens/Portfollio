7ca79c91b3cb00d7103a4be17a02883f927099d2
Wade-J-Hudgens <71951530+Wade-J-Hudgens@users.noreply.github.com>
2021-12-16 07:39:49 -0800

    Add files via upload

---
+++ src/components/Content/Resume/Resume.js
@@ -0,0 +1,24 @@
+import React from "react";
+import * as Scroll from 'react-scroll';
+import { Link, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll';
+
+import "./Resume.css";
+
+class Resume extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <div id="RESUME">
+                <Element name="resume"></Element>
+                <h1 className="Page_Header">Resume</h1>
+                <br />
+                <a id="RESUME_DOWNLOAD_BUTTON" href="Resume.pdf" download="WadeHudgens_Resume.pdf">Download Resume</a>
+            </div>
+        );
+    }
+}
+
+export default Resume;
